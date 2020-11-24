8384c5819ca7d41582b30af82b72ea7e6c2adab1
IvanIvchenko <32459492+IvanIvchenko@users.noreply.github.com>
2020-11-24 22:16:31 +0200

    update db credentials

--- blogger/__init__.py
+++ blogger/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 app.secret_key = 'hello_world'
 app.config.from_object(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://bdanikhbpqxuzr:c141409d09f88fad7a9225f8dcbd9fbc1740cb90c649a38856ba99e2f24900ef@ec2-54-86-170-8.compute-1.amazonaws.com:5432/db80h2uk7jbvk5'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://iyjbjjgvjpgeey:df0e9b0ec6e57cd7fb651c6516b35e15d85969a147d06e1bb1a00439d2649849@ec2-34-224-229-81.compute-1.amazonaws.com:5432/d1437q7nntmqbs'
 
 from blogger import models
 from blogger import views
